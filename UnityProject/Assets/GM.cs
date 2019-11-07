2e4fc84493246fb8b29db3bdbd519348e50844b7
yuyuOuO <55227155+yuyuOuO@users.noreply.github.com>
2019-11-07 14:42:22 +0800

    期中

---
+++ UnityProject/Assets/GM.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class GM : MonoBehaviour {
+
+    public AudioMixer mixer;
+
+	public void SetSFX(float value)
+    {
+        mixer.SetFloat("SFX", value);
+    }
+
+}
